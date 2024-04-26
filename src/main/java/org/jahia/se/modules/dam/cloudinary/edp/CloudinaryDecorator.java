ac0d299983c35e19f5fd01370909d63f16d68b78
hduchesne <hduchesne@jahia.com>
2024-04-26 10:50:56 +0200

    8.2 ready, last update is to remove 8.2-SNAPSHOT

--- src/main/java/org/jahia/se/modules/dam/cloudinary/edp/CloudinaryDecorator.java
+++ src/main/java/org/jahia/se/modules/dam/cloudinary/edp/CloudinaryDecorator.java
@@ -37,38 +37,42 @@ public class CloudinaryDecorator extends JCRNodeDecorator {
         }
     }
 
-    public String getUrl(List<String> params) throws RepositoryException {
-        if(this.isNodeType(CONTENT_TYPE_IMAGE)){
-            List<String> cloudyParams = new ArrayList<>();
-            cloudyParams.add("f_auto");
-            for (String param : params) {
-                if (param.startsWith("width:")) {
-                    String width = StringUtils.substringAfter(param, "width:");
-                    if(width.trim().isEmpty()){
-                        width = URL_WIDTH; //default width
+    public String getUrl(List<String> params) {
+        try {
+            if (this.isNodeType(CONTENT_TYPE_IMAGE)) {
+                List<String> cloudyParams = new ArrayList<>();
+                cloudyParams.add("f_auto");
+                for (String param : params) {
+                    if (param.startsWith("width:")) {
+                        String width = StringUtils.substringAfter(param, "width:");
+                        if (width.trim().isEmpty()) {
+                            width = URL_WIDTH; //default width
+                        }
+                        cloudyParams.add("w_" + width);
+                    } else if (param.startsWith("height:")) {
+                        String height = StringUtils.substringAfter(param, "height:");
+                        if (!height.trim().isEmpty())
+                            cloudyParams.add("h_" + height);
                     }
-                    cloudyParams.add("w_" + width);
-                } else if (param.startsWith("height:")) {
-                    String height = StringUtils.substringAfter(param, "height:");
-                    if(!height.trim().isEmpty())
-                        cloudyParams.add("h_" + height);
                 }
-            }
-            try {
-                StringBuilder sb = new StringBuilder();
-                sb.append(node.getProperty("cloudy:baseUrl").getString());
-                sb.append("/").append(StringUtils.join(cloudyParams, ",")).append("/");
-                if (node.hasProperty("cloudy:poster")) {
-                    sb.append(node.getProperty("cloudy:poster").getString());
-                } else if (node.hasProperty("cloudy:endUrl")) {
-                    sb.append(node.getProperty("cloudy:endUrl").getString());
-                }
+                try {
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(node.getProperty("cloudy:baseUrl").getString());
+                    sb.append("/").append(StringUtils.join(cloudyParams, ",")).append("/");
+                    if (node.hasProperty("cloudy:poster")) {
+                        sb.append(node.getProperty("cloudy:poster").getString());
+                    } else if (node.hasProperty("cloudy:endUrl")) {
+                        sb.append(node.getProperty("cloudy:endUrl").getString());
+                    }
 
-                return sb.toString();
-            } catch (RepositoryException e) {
-                return super.getUrl();
+                    return sb.toString();
+                } catch (RepositoryException e) {
+                    return super.getUrl();
+                }
+            } else {
+                return this.getUrl();
             }
-        }else{
+        }catch (RepositoryException e) {
             return this.getUrl();
         }
     }
